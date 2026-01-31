d2cb3a2c3389a59fec7e98432a503cdb8ef847d7
Tomas <tomastru114@gmail.com>
2026-01-31 21:58:58 +0100

    More basic stuff

--- Basic/src/main.rs
+++ Basic/src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    //scope
+
+    let x = 4;
+    println!("x is: {}", x); //Result = 4
+
+    {
+        let x = x - 2;
+        println!("x is: {}", x); //Result = 2
+    }
+
+    let x = x + 1;
+    println!("x is: {}", x); // Result = 4
+
+    //Get inputs
+
+    let mut inpt = String::new();
+
+    io::stdin().read_line(&mut inpt).expect("failed to read line");
+    println!("{}", inpt);
+
+    //Call a fn
+
+    test_one(); 
+
+    let number ={
+        let x = 3;
+        x + 1;
+    };
+}
+
+fn test_one(){
+    println!("Test");
 }
